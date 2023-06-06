8ed6e8245d58fde22ac2be4ac49b17d4f59721cb
John Casingal <jreydc@gmail.com>
2023-06-06 12:28:17 +0800

    Login Feature
    - created a table (tblUser)
    - integrated a connection structure
    - validation of username and password
    - transition to main form

--- CCSPayrollBillingSystem/Login.cs
+++ CCSPayrollBillingSystem/Login.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CCSPayrollBillingSystem
 {
@@ -12,32 +14,45 @@ namespace CCSPayrollBillingSystem
         private void btnLogin_Click(object sender, System.EventArgs e)
         {
             FormMain formMain = new FormMain();
-            formLogin formLogin = new formLogin();
-            db_CCSPayrollBillingEntities db_Entities = new db_CCSPayrollBillingEntities();
-            
-            //if (txtUsername.Text!=string.Empty || txtPassword.Text!= string.Empty)
-            //{
-            //    var userName = db_Entities.tblUsers.Find(txtUsername.Text);
-            //    var passWord = db_Entities.tblUsers.Find(txtPassword.Text);
-            //    if (userName == null)
-            //    {
-            //        MessageBox.Show("Null Values");
-            //    }
-            //    else
-            //    {
-            //        if (userName.Equals(txtUsername.Text) && passWord.Equals(txtPassword.Text))
-            //        {
-            //            formLogin.Hide();
-            //            formMain.Show();
-            //        }
-            //    }
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Please Enter Username and Password!");
-            //}
 
-            formMain.Show();
+            string connetionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=db_PayrollBilling;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection cnn = new SqlConnection(connetionString);
+            cnn.Open();
+
+            string sql = "SELECT * FROM tblUser WHERE Username='"+txtUsername.Text+"'AND Password='"+txtPassword.Text+"'";
+
+            SqlCommand command = new SqlCommand(sql, cnn);
+            SqlDataReader dataReader = command.ExecuteReader();
+
+            if (dataReader.Read())
+            {
+                MessageBox.Show("Hi! " + dataReader.GetValue(1));
+                dataReader.Close();
+                this.Hide();
+                formMain.ShowDialog();
+            }
+            else
+            {
+                dataReader.Close();
+                MessageBox.Show("No Accounts Available!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            command.Dispose();
+            cnn.Close();
+        
         }
+        //SELECT QUERY STRUCTURE
+
+        //string sql = "Select * from tblUser";
+
+        //SqlCommand command = new SqlCommand(sql, cnn);
+        //SqlDataReader dataReader = command.ExecuteReader();
+
+        //while (dataReader.Read())
+        //{
+        //    Output = Output + dataReader.GetValue(0) + " - " + dataReader.GetValue(1) +"-"+ dataReader.GetValue(2) + " - " + dataReader.GetValue(3) + " - " + dataReader.GetValue(4) + "\n";
+        //}
+
+        //MessageBox.Show("Connection Open !\n"+ Output);
     }
 }
